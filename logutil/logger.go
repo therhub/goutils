73c0082f559006da9d0bd3921c9f89708db94b41
Ther <1123979345@qq.com>
2020-05-12 17:43:13 +0800

    修改logutil

--- logutil/logger.go
+++ logutil/logger.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"os"
 	"time"
+
+	"github.com/therhub/goutils/stringutil"
 )
+
+var logpath string
 
 // write log to file
 // m is module Name,
@@ -15,11 +19,15 @@ func write(m, lt, s string) error {
 	// get current time
 	t := time.Now()
 
+	if logpath == "" {
+		logpath = "./"
+	}
+
 	// get file name
-	f := fmt.Sprintf("./log%s.log", t.Format("2006010215"))
+	f := fmt.Sprintf("%slog%s.log", logpath, t.Format("2006010215"))
 
 	// get log format
-	out := fmt.Sprintf("\r\nid:%s\r\ntimestamp:%v;\r\nmodule:%s;\r\nlogtype:%s;\r\ntime:%v;\r\ncontent:%v;\r\n", t.Unix(), m, lt, t.Format("2006-01-02 15:04:05"), s)
+	out := fmt.Sprintf("\r\n#id:%v\r\n#timestamp:%v;\r\n#module:%s;\r\n#logtype:%s;\r\n#time:%v;\r\n#content:%v;\r\n", stringutil.NewID(), t.Unix(), m, lt, t.Format("2006-01-02 15:04:05"), s)
 
 	file, err := os.OpenFile(f, os.O_APPEND|os.O_CREATE, os.ModePerm)
 
@@ -37,7 +45,7 @@ func write(m, lt, s string) error {
 
 // set log path
 func SetLogPath(p string) {
-
+	logpath = p
 }
 
 // system log
--- logutil/logger_test.go
+++ logutil/logger_test.go
@@ -25,3 +25,30 @@ func Test_write(t *testing.T) {
 		})
 	}
 }
+
+func Test_write_setPath(t *testing.T) {
+
+	SetLogPath("D:\\log\\")
+
+	type args struct {
+		m  string
+		lt string
+		s  string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+		{"test1", args{m: "sys", lt: "error", s: "this is a test for error"}, false},
+		{"test2", args{m: "sys", lt: "info", s: "this is a test for info"}, false},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if err := write(tt.args.m, tt.args.lt, tt.args.s); (err != nil) != tt.wantErr {
+				t.Errorf("write() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
+}
